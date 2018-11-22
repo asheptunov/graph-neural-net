70687e97617a23e2b55e6fda3e6738a54d084263
Andriy Sheptunov <andriy99@uw.edu>
2018-11-21 19:09:47 -0800

    Added validation, stats outputting, and training framework
    Signed-off-by: Andriy Sheptunov <andriy99@uw.edu>

--- src/NeuralNetTrainer.java
+++ src/NeuralNetTrainer.java
@@ -1,3 +1,5 @@
+import java.io.OutputStream;
+import java.io.PrintStream;
 import java.util.*;
 
 /**
@@ -13,25 +15,70 @@ public class NeuralNetTrainer {
 	private List<double[]> dataSampler;
 	private Random random;
 
+	private PrintStream observer;
+
 	/**
 	 * Creates a new trainer for the specified neural network, designating the given data map as the training data for
-	 * the net. Map is assumed to be non-null, and map input vectors of the net's input dimension to expected output
+	 * the net. Registers the specified stream (or null if no observer is needed) as the observer for outputting
+	 * statistics during training when requested.
+	 * Map is assumed to be non-null, and map input vectors of the net's input dimension to expected output
 	 * vectors of the net's output dimension. Network is assumed to non-null and untrained.
 	 * DO NOT PIPE IN VALIDATION OR TESTING PARTITIONS. ONLY USE TRAINING PARTITION.
 	 *
 	 * @param data training data map from input vectors to expected output vectors
 	 * @param net the network to train
 	 */
-	public NeuralNetTrainer(Map<double[], double[]> data, NeuralNet net) {
+	public NeuralNetTrainer(Map<double[], double[]> data, NeuralNet net, PrintStream observer) {
 		assert data != null;
 		assert net != null;
 		assert data.keySet().iterator().next().length == net.getInputDim();
 		assert data.values().iterator().next().length == net.getOutputDim();
+		dataMaster = new HashMap<>();
+		dataSampler = new ArrayList<>();
 		for (double[] input : data.keySet()) {
 			dataMaster.put(input, data.get(input));
 		}
 		refillSampler();
+		this.observer = observer; // null is ok
 		random = new Random(1);
+	}
+
+	/**
+	 * Trains the neural network using a specified number of gradient descent steps of the specified size, drawing a
+	 * randomly selected batch of the specified size each time a step is performed. Records intermittent statistics
+	 * to the observer stream, if observing has been requested and an observer exists.
+	 *
+	 * @param iterations the amount of gradient descent iterations to perform
+	 * @param stepSize   the scale factor for weight adjustment
+	 * @param batchSize  the size of training batches to draw per gradient descent iteration
+	 * @param observed   whether or not to output intermittent statistics during training
+	 */
+	public void train(int iterations, int stepSize, int batchSize, boolean observed) {
+		int validationSize = dataMaster.size() / 100 + 1; // at least 0
+		for (int i = 0; i < iterations; i++) {
+			net.gradientStep(sample(batchSize), stepSize);
+			if (observed && observer != null) {
+				observer.printf("Error after step %d: %.2f\n", i, validate(validationSize));
+			}
+		}
+	}
+
+	/**
+	 * Tests and returns the net's estimated average loss using a batch of specified size of training data. Estimate not
+	 * guaranteed to be within any specific degree of accuracy of the net's true average error over all samples. Batch
+	 * size assumed to be positive and upper bounded by the total size of the input data.
+	 *
+	 * @return the net's estimated average loss
+	 */
+	public double validate(int batchSize) {
+		assert batchSize > 0 && batchSize < dataMaster.size();
+		double loss = 0;
+		Iterator<double[]> keysIterator = dataMaster.keySet().iterator();
+		for (int i = 0; i < batchSize; i++) {
+			double[] sample = keysIterator.next();
+			loss += net.calculateLoss(sample, dataMaster.get(sample));
+		}
+		return loss / batchSize;
 	}
 
 	/**
@@ -64,6 +111,15 @@ public class NeuralNetTrainer {
 		dataSampler.clear();
 		assert dataSampler.isEmpty();
 		dataSampler.addAll(dataMaster.keySet());
+	}
+
+	/**
+	 * Returns the neural network that this trainer operates on.
+	 *
+	 * @return the neural net
+	 */
+	private NeuralNet getNeuralNet() {
+		return net;
 	}
 
 }
